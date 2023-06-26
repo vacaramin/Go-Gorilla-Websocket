a26338cd6d3e2c48b52891c79374c2dc2fa4be9c
Waqar Amin <vacaramin86@gmail.com>
2023-06-27 01:48:57 +0500

    setup Routes initialized

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage")
+}
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+}
 
 func main() {
 	fmt.Println("Go Websocket")
