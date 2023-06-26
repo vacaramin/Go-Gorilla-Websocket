632b5e05dacea54cf76c680cb0a9a0f3b1ca367b
Waqar Amin <vacaramin86@gmail.com>
2023-06-27 02:09:19 +0500

    Web socket endpoint function initialized

--- main.go
+++ main.go
@@ -8,8 +8,13 @@ import (
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage")
 }
+func wsEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Websocket End Point")
+
+}
 func setupRoutes() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndPoint)
 }
 
 func main() {
