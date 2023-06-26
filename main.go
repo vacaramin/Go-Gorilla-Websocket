46879de3e0ca93a242e52e6d7c7042d0095a2647
Waqar Amin <vacaramin86@gmail.com>
2023-06-27 02:12:29 +0500

    Web socket connection Upgrade function written

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage")
 }
 func wsEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Websocket End Point")
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
 
 }
 func setupRoutes() {
